ee9ba37e2820cc51419d69f19bc1839246440f5e
KiranThumar <thummarkiran@gmail.com>
2021-12-02 14:51:16 -0700

    first commit

---
+++ src/main/java/quclickacademy/Mavenjava/Mavenjava/AppiumTest.java
@@ -0,0 +1,18 @@
+package quclickacademy.Mavenjava.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+	@Test
+	public void AndroidApp()
+	{
+		
+	}
+	@Test
+	public void IOSApp()
+	{
+		
+	}
+	
+}
---
+++ src/main/java/quclickacademy/Mavenjava/Mavenjava/RestAPITest.java
@@ -0,0 +1,17 @@
+package quclickacademy.Mavenjava.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class RestAPITest {
+
+	@Test
+	public void PostJira()
+	{
+		
+	}
+	@Test
+	public void DeleteTwitter()
+	{
+		
+	}
+}
